5db6b0b3f37728c53a517ff6954d4a2521d40537
Kamen Dinev <kamen.b.dinev@gmail.com>
2024-12-08 10:09:01 +0200

    use house and ballon

--- vite.config.js
+++ vite.config.js
@@ -3,25 +3,26 @@ import handlebars from 'vite-plugin-handlebars';
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 export default {
-  plugins: [
-    handlebars({
-      context: {
-        "xs-max": 623,
-        "sm-min": 624
-      },
-      partialDirectory: resolve(__dirname, 'src/partials'),
-    }),
-    ViteImageOptimizer({
-      png: { quality: 80 },
-      jpeg: { quality: 75 },
-      webp: { quality: 80 },
-      avif: { quality: 70 },
-      // svg: {
-      //   plugins: [
-      //     { name: 'removeViewBox', active: false },
-      //     { name: 'sortAttrs' },
-      //   ],
-      // },
-    }),
-  ],
+    base: "/harmony-web/",
+    plugins: [
+        handlebars({
+            context: {
+                "xs-max": 623,
+                "sm-min": 624
+            },
+            partialDirectory: resolve(__dirname, 'src/partials'),
+        }),
+        ViteImageOptimizer({
+        png: { quality: 80 },
+        jpeg: { quality: 75 },
+        webp: { quality: 80 },
+        avif: { quality: 70 },
+        // svg: {
+        //   plugins: [
+        //     { name: 'removeViewBox', active: false },
+        //     { name: 'sortAttrs' },
+        //   ],
+        // },
+        }),
+    ],
 };
